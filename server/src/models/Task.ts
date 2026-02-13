0a6cf16f57f495aefe468e0296110075601a427e
Devkdy22 <devkdy99@gmail.com>
2026-02-13 14:22:24 +0900

    [back]태스크 모델: 상태 필드의 값 변경 및 선택적 속성 추가

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -2,20 +2,26 @@ import mongoose, { Schema, Document, Types } from "mongoose";
 
 export interface ITask extends Document {
   projectId: Types.ObjectId;
+  userId: Types.ObjectId;
   title: string;
-  status: "할 일" | "진행 중" | "완료";
-  dueDate: Date;
+  status: "todo" | "doing" | "done";
+  dueDate?: Date;
+  memo?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const TaskSchema = new Schema<ITask>({
   projectId: { type: Schema.Types.ObjectId, ref: "Project", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   status: {
     type: String,
-    enum: ["할 일", "진행 중", "완료"],
-    default: "할 일",
+    enum: ["todo", "doing", "done"],
+    default: "todo",
   },
   dueDate: { type: Date },
-});
+  memo: { type: String },
+}, { timestamps: true });
 
 export default mongoose.model<ITask>("Task", TaskSchema);
