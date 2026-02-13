d2d86bfa6fb892e469c70e3058bca4f2f3f0c86a
Devkdy22 <devkdy99@gmail.com>
2026-02-13 14:21:38 +0900

    [back]프로젝트 모델: createdAt 필드를 선택적 속성으로 변경

--- server/src/models/Project.ts
+++ server/src/models/Project.ts
@@ -6,20 +6,23 @@ export interface IProject extends Document {
   description: string;
   category: string;
   priority: "낮음" | "보통" | "높음";
-  createdAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const ProjectSchema = new Schema<IProject>({
-  userId: {
-    type: Schema.Types.ObjectId, //String → ObjectId
-    ref: "User", //User와 관계
-    required: true,
+const ProjectSchema = new Schema<IProject>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId, //String → ObjectId
+      ref: "User", //User와 관계
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: { type: String },
+    category: { type: String },
+    priority: { type: String, enum: ["낮음", "보통", "높음"], default: "보통" },
   },
-  title: { type: String, required: true },
-  description: { type: String },
-  category: { type: String },
-  priority: { type: String, enum: ["낮음", "보통", "높음"], default: "보통" },
-  createdAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model<IProject>("Project", ProjectSchema);
